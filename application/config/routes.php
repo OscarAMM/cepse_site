9d73fddffc936e99d5312dad8892ae3a83f70720
Oscar Monsreal <A15216352@alumnos.uady.mx>
2021-04-13 15:56:35 -0500

    Auth community

--- application/config/routes.php
+++ application/config/routes.php
@@ -53,3 +53,5 @@ $route['default_controller'] = 'welcome';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
 $route['Welcome/index_cepse']='index';
+/**Login route for public section*/
+$route[LOGIN_PAGE] = 'examples/login';
--- application/controllers/Homex.php
+++ application/controllers/Homex.php
@@ -6,15 +6,20 @@ class Homex extends CI_Controller
         parent::__construct();
         $this->load->database();
         $this->load->helper('url_helper');
+        $this->load->helper('form');
     }
     public function index()
     {
         $this->load->model('Home');
         $this->load->view('home/index_cepse');
     }
+    public function form_home()
+    {
+        $this->load->view('home/store');
+    }
     public function store()
     {
-        //
+        
     }
     public function delete()
     {
