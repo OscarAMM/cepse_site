2a46be0ef3a94fe6014fd56ebd3d72cdd965b195
Oscar Monsreal <A15216352@alumnos.uady.mx>
2021-05-11 10:17:28 -0500

    Login Controller. Feature: Index and construct methods created

---
+++ application/controllers/Login.php
@@ -0,0 +1,21 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Login extends CI_Controller
+{
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->model('Users');
+        $this->load->library('form_validation');
+    }
+    public function index()
+    {
+        $this->load->view('users/login');
+    }
+    public function validate()
+    {
+        
+    }
+}
