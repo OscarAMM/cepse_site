b0318109d00d3df75dce2e69e6e68db6bcf01892
Oscar Monsreal <A15216352@alumnos.uady.mx>
2021-05-12 13:00:51 -0500

    Login Controller. Validation function finished and working

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -12,6 +12,7 @@ class Login extends CI_Controller
         $this->load->helper(array(
             'login_rules',
         ));
+        $this->load->model('Auth');
     }
     public function index()
     {
@@ -22,9 +23,22 @@ class Login extends CI_Controller
         $rules = getLoginRules();
         $this->form_validation->set_rules($rules);
         if ($this->form_validation->run() === false) {
-            $this->load->view('users/login');
+            $errors = array(
+                'email' => form_error('email'),
+                'password' => form_error('password'),
+            );
+            echo json_encode($errors);
+            $this->output->set_status_header(400);
         } else {
-
+            $user = $this->input->post('email');
+            $password = $this->input->post('password');
+            $response = $this->Auth->login($user, $password);
+            if (!$response) {
+                echo json_encode(array('msg' => 'Las credenciales no coinciden con ninguno registrado. Intente nuevamente.'));
+                $this->output->set_status_header(401);
+                exit;
+            }
+            echo json_encode(array('msg' => 'Bienvenido'));
         }
     }
 }
