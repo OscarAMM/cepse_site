3b6ba037c86b5ce172889e5bd73ac81e2ae53cae
Oscar Monsreal <A15216352@alumnos.uady.mx>
2021-05-12 13:03:12 -0500

    Auth Model [finished]: Login function [database credential  verification] finished

---
+++ application/models/Auth.php
@@ -0,0 +1,16 @@
+<?php
+class Auth extends CI_Model
+{
+    public function __construct()
+    {
+        $this->load->database();
+    }
+    public function login($user, $password)
+    {
+        $data = $this->db->get_where('users', array('email' => $user, 'password' => $password), 1);
+        if (!$data->result()) {
+            return false;
+        }
+        return $data->result();
+    }
+}
