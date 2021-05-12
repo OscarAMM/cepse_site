98845f15f43893de2f187f5aa425d0c29ce21160
Oscar Monsreal <A15216352@alumnos.uady.mx>
2021-05-12 13:02:21 -0500

    Login view [style]: Validation style finished

--- application/views/users/login.php
+++ application/views/users/login.php
@@ -5,17 +5,17 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login</title>
+    <title>UEPSE | Login</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css"
         integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
-        integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous">
-    </script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous">
     </script>
     <!---Fontawesome--->
     <script src="https://kit.fontawesome.com/8efbe29298.js" crossorigin="anonymous"></script>
+    <link rel="shortcut icon" href="<?php echo base_url('assets/img/favicon.ico'); ?>" type="image/x-icon">
 </head>
 
 <body>
@@ -42,7 +42,7 @@
                 <div class="card shadow mb-3">
                     <div class="row no-gutters">
                         <div class="col-md-3">
-                            <img src="<?php echo base_url(); ?>assets/img/banner_login.jpg" width="540px;"
+                            <img src="<?php echo base_url('assets/img/banner_login.jpg'); ?>" width="540px;"
                                 class="img-fluid" alt="banner_side">
                         </div>
                         <div class="col-md-9">
@@ -51,17 +51,27 @@
                                 <h5 class="card-title text-monospace">Unidad Estatal de Participación Social en la
                                     Educación</h5>
                                 <div>
-                                    <form action="<?php echo base_url('Login/validate'); ?>" method="post">
-                                        <div class="form-group">
+                                    <form action="<?php echo base_url('Login/validate'); ?>" method="post"
+                                        id="login_form">
+                                        <div class="form-group" id="email_group">
                                             <label for="user">Usuario</label>
-                                            <input type="text" name="identity" id="username" class="form-control">
+                                            <input type="text" name="email" id="email" class="form-control" required autocomplete="off">
+                                            <div class="invalid-feedback">
+                                                Please select a valid state.
+                                            </div>
                                         </div>
-                                        <div class="form-group">
+                                        <div class="form-group" id="password_group">
                                             <label for="password">Password</label>
-                                            <input type="password" name="password" id="password" class="form-control">
+                                            <input type="password" name="password" id="password" class="form-control" required autocomplete="off">
+                                            <div class="invalid-feedback">
+                                                Please select a valid state.
+                                            </div>
                                         </div>
                                         <div class="form-group">
-                                            <button type="submit" class="btn btn-primary">Iniciar sesión</button>
+                                            <button type="submit" class="btn btn-primary">Ingresar</button>
+                                        </div>
+                                        <div class="form-group" id="alert">
+
                                         </div>
                                     </form>
                                 </div>
@@ -72,6 +82,8 @@
             </div>
         </div>
     </main>
+
+    <script src="<?php echo base_url('assets/js/login.js') ?>"></script>
 </body>
 
 </html>
