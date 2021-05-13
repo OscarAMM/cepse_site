511e778f03b29de7f87f1540b87d126f6ae1b7ef
Oscar Monsreal <A15216352@alumnos.uady.mx>
2021-05-13 11:24:09 -0500

    User migration [hotfix]: Fixed data type

--- application/migrations/001_create_user.php
+++ application/migrations/001_create_user.php
@@ -1,45 +1,50 @@
 <?php
 
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
-class Migration_create_user extends CI_Migration {
+class Migration_create_user extends CI_Migration
+{
 
-        public function up()
-        {
-                $this->dbforge->add_field(array(
-                        'user_id' => array(
-                                'type' => 'INT',
-                                'constraint' => 5,
-                                'unsigned' => TRUE,
-                                'auto_increment' => TRUE
-                        ),
-                        'username' => array(
-                                'type' => 'VARCHAR',
-                                'constraint' => '191',
-                                'null' => FALSE,
-                        ),
-                        'email' => array(
-                                'type' => 'VARCHAR',
-                                'constraint' => '191',
-                                'null' => FALSE,
-                        ),
-                        'password' => array(
-                            'type' => 'VARCHAR',
-                            'constraint' => '191',
-                            'null' => FALSE
-                        ),
-                        'status' => array(
-                            'type' => 'VARCHAR',
-                            'constraint'=> '255',
-                            'null' => FALSE
-                        )
-                ));
-                $this->dbforge->add_key('user_id', TRUE);
-                $this->dbforge->create_table('users');
-        }
+    public function up()
+    {
+        $this->dbforge->add_field(array(
+            'user_id' => array(
+                'type' => 'INT',
+                'constraint' => 5,
+                'unsigned' => true,
+                'auto_increment' => true,
+            ),
+            'username' => array(
+                'type' => 'VARCHAR',
+                'constraint' => '191',
+                'null' => false,
+            ),
+            'email' => array(
+                'type' => 'VARCHAR',
+                'constraint' => '191',
+                'null' => false,
+            ),
+            'password' => array(
+                'type' => 'VARCHAR',
+                'constraint' => '191',
+                'null' => false,
+            ),
+            'status' => array(
+                'type' => 'VARCHAR',
+                'constraint' => '255',
+                'null' => false,
+            ),
+            'range' => array(
+                'type' => 'VARCHAR',
+                'constraint' => '5',
+            ),
+        ));
+        $this->dbforge->add_key('user_id', true);
+        $this->dbforge->create_table('users');
+    }
 
-        public function down()
-        {
-                $this->dbforge->drop_table('users');
-        }
+    public function down()
+    {
+        $this->dbforge->drop_table('users');
+    }
 }
