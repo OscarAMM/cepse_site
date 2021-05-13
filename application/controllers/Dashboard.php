d4b2c9514ff7349b72c34cffde97ccc226bcf313
Oscar Monsreal <A15216352@alumnos.uady.mx>
2021-05-13 16:02:45 -0500

    Controller[Dashboard]:template managment

---
+++ application/controllers/Dashboard.php
@@ -0,0 +1,28 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+class Dashboard extends CI_Controller
+{
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->library(array('session'));
+    }
+    public function index(){
+        if($this->session->userdata('is_logged')){
+           $view = $this->load->view('users/index','', true);
+           $this->getTemplate($view);
+        }else{
+            redirect('login');
+        }
+    }
+    public function getTemplate($view){
+        $data = array(
+            'head' => $this->load->view('template/head', '', true),
+            'nav' => $this->load->view('template/nav','', true),
+            'aside' => $this->load->view('template/aside', '', true),
+            'content'=>$view,
+            'footer' => $this->load->view('template/footer', '', true),
+        );
+        $this->load->view('template/dashboard', $data);
+    }
+}
