c2ad16067c06f40e7384219c589dc0ae2f5a160b
Oscar Monsreal <A15216352@alumnos.uady.mx>
2021-05-13 11:25:59 -0500

    Login JS[hotfix]: Fixed redirect window

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,34 +1,37 @@
-(function ($) {
-    $("#login_form").submit(function (ev) {
+(function($){
+    $("#frm-login").submit(function(ev){
         $("#alert").html("");
-        ev.preventDefault();
         $.ajax({
-            url: 'validate',
+            url: 'login/validate',
             type: 'POST',
             data: $(this).serialize(),
-            success: function (data) {
-                let json = JSON.parse(data);
-                console.log(json);
+            success: function(err){
+                var json = JSON.parse(err);
+                window.location.replace(json.url)
             },
             statusCode: {
-                400: function (xhr) {
-                    $("#email_group > input").removeClass('is-invalid');
-                    $("#password_group > input").removeClass('is-invalid');
-                    let json = JSON.parse(xhr.responseText);
-                    if (json.email.length != 0) {
+                400: function(xhr){
+                    $("#email > input").removeClass("is-invalid");
+                    $("#password > input").removeClass("is-invalid");
+                    var json = JSON.parse(xhr.responseText);
+                    if(json.email.length != 0){
                         $("#email_group > div").html(json.email);
                         $("#email_group > input").addClass('is-invalid');
                     }
-                    if (json.password.length != 0) {
+                    if(json.password.length != 0){
                         $("#password_group > div").html(json.password);
                         $("#password_group > input").addClass('is-invalid');
-                    }
+                    }   
                 },
-                401: function (xhr) {
-                    let json = JSON.parse(xhr.responseText);
-                    $("#alert").html('<div class="alert alert-danger" role="alert" >' + json.msg + '</div>');
+                401: function(xhr){
+                    var json = JSON.parse(xhr.responseText);
+                    // console.log(json);
+                    $("#alert").html('<div class="alert alert-danger" role="alert">'+ json.msg +'</div>')
                 }
-            },
+                
+            }
+
         });
+        ev.preventDefault();
     });
-})(jQuery)
+})(jQuery);
