256d163e37331176b4e2081c02f61187bf0171a8
Oscar Monsreal <A15216352@alumnos.uady.mx>
2021-05-14 14:13:33 -0500

    Helper[user]: Email verification with unique

--- application/helpers/users/users_rules_helper.php
+++ application/helpers/users/users_rules_helper.php
@@ -14,10 +14,11 @@ if (!function_exists('getUserRules')) {
             array(
                 'field' => 'email',
                 'label' => 'Correo',
-                'rules' => 'required|valid_email|trim',
+                'rules' => 'required|valid_email|trim|is_unique[users.email]',
                 'errors' => array(
                     'required' => 'El campo %s no debe estar vacío',
                     'valid_email' => 'El %s debe ser válido',
+                    'is_unique' => 'El %s debe ser único. Ya se encuentra registrado dicho correo.',
                 ),
             ),
             array(
@@ -26,6 +27,14 @@ if (!function_exists('getUserRules')) {
                 'rules' => 'required|trim',
                 'errors' => array(
                     'required' => 'El campo %s no debe estar vacío.',
+                ),
+            ),
+            array(
+                'field' => 'password_c',
+                'label' => 'Confirmación de contraseña',
+                'rules' => 'required',
+                'errors' => array(
+                    'required' => 'El campo de %s no debe estar vacío.',
                 ),
             ),
             array(
