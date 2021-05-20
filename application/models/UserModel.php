25f56735f3d80d476ecd2ecd3d0cecbb12437dc9
Oscar Monsreal <A15216352@alumnos.uady.mx>
2021-05-20 12:19:51 -0500

    Model [user] (feature): Update function at data base created and fixed

--- application/models/UserModel.php
+++ application/models/UserModel.php
@@ -16,8 +16,28 @@ class UserModel extends CI_Model
             return false;
         }
     }
-    public function getUsers(){
+    public function getUsers()
+    {
         $sql = $this->db->get('users');
         return $sql->result();
+    }
+    public function getPaginate($limit, $offset)
+    {
+        $sql = $this->db->order_by('user_id', 'DESC')->get('users', $limit, $offset);
+        return $sql->result();
+    }
+    public function getUser($id)
+    {
+        $this->db->select();
+        $this->db->from('users');
+        $this->db->where('user_id', $id);
+        $query = $this->db->get();
+        return $query->row();
+    }
+    public function updateUser($id, $user)
+    {
+        $this->db->where('user_id', $id);
+        $this->db->update('users', $user);
+        return $this->db->insert_id();
     }
 }
