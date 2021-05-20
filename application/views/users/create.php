da398d4c45416a477670da78b5cebfb6ee0e1863
Oscar Monsreal <A15216352@alumnos.uady.mx>
2021-05-20 12:23:41 -0500

    Views[users/create]: Form completed

--- application/views/users/create.php
+++ application/views/users/create.php
@@ -1,4 +1,16 @@
 <div class="container">
+    <!-- error --->
+    <?php if(validation_errors()):?>
+    <div class="alert alert-danger alert-dismissible fade show" role="alert">
+        <h1 class="alert-heading">¡Error!</h1>
+        <p>Parece que tienes algunos errores:</p>
+        <?php echo validation_errors(); ?>
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+        </button>
+    </div>
+    <?php endif?>
+    <!---- end error --->
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card shadow-sm my-3">
