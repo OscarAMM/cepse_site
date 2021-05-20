cc98e3a61931d80897d43b9f2229d7ee85adb870
Oscar Monsreal <A15216352@alumnos.uady.mx>
2021-05-20 12:13:56 -0500

    Controller [User] (Feature): Update function created and tested

--- application/controllers/Users.php
+++ application/controllers/Users.php
@@ -5,18 +5,46 @@ class Users extends CI_Controller
     public function __construct()
     {
         parent::__construct();
-        $this->load->library(array('session', 'form_validation'));
+        $this->load->library(array('session', 'form_validation', 'pagination'));
         $this->load->helper(array('users/users_rules'));
         $this->load->model('UserModel');
         if (!$this->session->userdata('is_logged')) {
             redirect('login');
         }
     }
-    public function index()
+    public function index($offset = 0)
     {
         $data = $this->UserModel->getUsers();
-        $this->getTemplate($this->load->view('users/index', array('data'=>$data),true));
-        
+        /**
+         * Pagination configuration [pagination-tag] = Bootstrap classes
+         */
+        $config['base_url'] = base_url('users/index');
+        $config['per_page'] = 20;
+        $config['full_tag_open'] = '<div class="text-center"> <nav> <ul class="pagination">';
+        $config['full_tag_close'] = '</ul></nav></div>';
+        $config['num_tag_open'] = '<li class="page-item"><span class="page-link">';
+        $config['num_tag_close'] = '</span></li>';
+        $config['cur_tag_open'] = '<li class="page-item active"> <span class="page-link">';
+        $config['cur_tag_close'] = '<span class="sr-only">(current)</span></span></li>';
+        $config['next_tag_open'] = '<li class="page-item"> <span class="page-link">';
+        $config['next_tag_close'] = '<span aria-hidden="true">&raquo;</span></span></li>';
+        $config['prev_tag_open'] = '<li class="page-item"><span class="page-link">';
+        $config['prev_tag_close'] = '</span></li>';
+        $config['first_tag_open'] = '<li class="page-item"><span class="page-link">';
+        $config['first_tag_close'] = '</span></li>';
+        $config['last_tag_open'] = '<li class="page-item"><span class="page-link">';
+        $config['last_tag_close'] = '</span></li>';
+        $config['total_rows'] = count($data);
+        $this->pagination->initialize($config);
+        /**
+         * gets data from getPaginate function at user model
+         */
+        $page = $this->UserModel->getPaginate($config['per_page'], $offset);
+        /**
+         * Data pagination passed to template
+         */
+        $this->getTemplate($this->load->view('users/index', array('data' => $page), true));
+
     }
     public function create()
     {
@@ -25,39 +53,86 @@ class Users extends CI_Controller
     }
     public function store()
     {
-        $rules = getUserRules();
-        //form fields
-        $username = $this->input->post('username');
-        $email = $this->input->post('email');
-        $password = $this->input->post('password');
-        $confirm_password = $this->input->post('password_c');
-        $range = $this->input->post('range');
-        $view = $this->load->view('users/create', '', true);
-        //validation rules (at helpers/users)
-        $this->form_validation->set_rules($rules);
-        //verification at form action
-        if ($this->form_validation->run() === false) {
-            $this->output->set_status_header(500);
+        if ($this->input->server('REQUEST_METHOD') === 'POST') {
+            $rules = getUserRules();
+            /**
+             * Form fields inputs
+             */
+            $username = $this->input->post('username');
+            $email = $this->input->post('email');
+            $password = $this->input->post('password');
+            $confirm_password = $this->input->post('password_c');
+            $range = $this->input->post('range');
             $view = $this->load->view('users/create', '', true);
-            $this->load->view('messages/error');
-            $this->getTemplate($view);
-        } else {
-            //user data session
-            $user = array(
-                'username' => $username,
-                'email' => $email,
-                'password' => $password,
-                'range' => $range,
-                'status' => 'activo',
-            );
-            //insert data into users table
-            if (!$this->UserModel->save($user)) {
+            //validation rules (at helpers/users)
+            $this->form_validation->set_rules($rules);
+            //verification at form action
+            if ($this->form_validation->run() === false) {
                 $this->output->set_status_header(500);
+                $view = $this->load->view('users/create', '', true);
+                //$this->load->view('messages/error');
+                $this->getTemplate($view);
             } else {
-                //data session
-                $message = $this->session->set_flashdata('msg', 'El usuario ha sido registrado con éxito.');
-                redirect(base_url('users/index'));
+                //user data session
+                $user = array(
+                    'username' => $username,
+                    'email' => $email,
+                    'password' => $password,
+                    'range' => $range,
+                    'status' => 'activo',
+                );
+                //insert data into users table
+                if (!$this->UserModel->save($user)) {
+                    $this->output->set_status_header(500);
+                } else {
+                    //data session
+                    $message = $this->session->set_flashdata('msg', 'El usuario ha sido registrado con éxito.');
+                    redirect(base_url('users/index'));
+                }
             }
+        } else {
+            show_404();
+        }
+
+    }
+    public function edit($id = 0)
+    {
+        $user = $this->UserModel->getUser($id);
+        $view = $this->load->view('users/edit', array('user' => $user), true);
+        $this->getTemplate($view);
+    }
+    public function update($id)
+    {
+        if ($this->input->server('REQUEST_METHOD') === 'POST') {
+            $user = $this->UserModel->getUser($id);
+            $rules = getUpdateUserRules();
+            /**
+             * Form fields inputs
+             */
+            $username = $this->input->post('username');
+            $email = $this->input->post('email');
+            $range = $this->input->post('range');
+            $view = $this->load->view('users/edit', '', true);
+            //validation rules (at helpers/users)
+            $this->form_validation->set_rules($rules);
+            if ($this->form_validation->run() === false) {
+                $this->output->set_status_header(500);
+                $view = $this->load->view('users/edit', array('user' => $user), true);
+                $this->getTemplate($view);
+            } else {
+                //user data session
+                $user = array(
+                    'username' => $username,
+                    'email' => $email,
+                    'range' => $range,
+                    'status' => 'activo',
+                );
+                $this->UserModel->updateUser($id, $user);
+                $this->session->set_flashdata('msg', 'El usuario ' . $username . ' ha sido actualizado correctamente.');
+                redirect('users');
+            }
+        } else {
+            show_404();
         }
     }
     public function getTemplate($view)
