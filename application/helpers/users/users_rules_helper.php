09d6628c46393765a0f0868c52d0d58aa965fc6d
Oscar Monsreal <A15216352@alumnos.uady.mx>
2021-05-24 11:06:33 -0500

    Helper [User]: Password and profile helper created

--- application/helpers/users/users_rules_helper.php
+++ application/helpers/users/users_rules_helper.php
@@ -1,4 +1,5 @@
 <?php
+//update
 if (!function_exists('getUpdateUserRules')) {
     function getUpdateUserRules()
     {
@@ -32,6 +33,7 @@ if (!function_exists('getUpdateUserRules')) {
         );
     }
 }
+//create
 if (!function_exists('getUserRules')) {
     function getUserRules()
     {
@@ -76,6 +78,56 @@ if (!function_exists('getUserRules')) {
                 'rules' => 'required',
                 'errors' => array(
                     'required' => 'El campo %s no debe estar vacío.',
+                ),
+            ),
+        );
+    }
+}
+//password
+if (!function_exists('getUserPassword')) {
+    function getUserPassword()
+    {
+        return array(
+            array(
+                'field' => 'password',
+                'label' => 'Contraseña',
+                'rules' => 'required',
+                'errors' => array(
+                    'required' => 'El campo %s no debe estar vacío',
+                ),
+            ),
+            array(
+                'field' => 'confirm_password',
+                'label' => 'confirmación de contraseña',
+                'rules' => 'required|matches[password]',
+                'errors' => array(
+                    'required' => 'El campo %s no debe estar vacío',
+                    'matches' => 'No coincide las contraseñas',
+                ),
+            ),
+        );
+    }
+}
+//profile
+if (!function_exists('getUserProfile')) {
+    function getUserProfile()
+    {
+        return array(
+            array(
+                'field' => 'username',
+                'label' => 'Nombre de usuario',
+                'rules' => 'required',
+                'errors' => array(
+                    'required' => 'El campo %s no debe estar vacío',
+                ),
+            ),
+            array(
+                'field' => 'email',
+                'label' => 'Correo',
+                'rules' => 'required|valid_email',
+                'errors' => array(
+                    'required' => 'El campo %s no debe estar vacío',
+                    'valid_email' => 'El dato de %s debe ser válido',
                 ),
             ),
         );
