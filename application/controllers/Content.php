25466a4c9b83c24ea916e28a38a8c121b8862ed5
Oscar Monsreal <A15216352@alumnos.uady.mx>
2021-05-24 15:03:44 -0500

    Controller [Content]: Base controller functions

--- application/controllers/Content.php
+++ application/controllers/Content.php
@@ -5,12 +5,37 @@ class Content extends CI_Controller
     public function __construct()
     {
         parent::__construct();
-        $this->load->database();
-        $this->load->helper('url_helper');
-        $this->load->helper('form');
-        $this->load->library('ion_auth');
-        if (!$this->ion_auth->logged_in()) {
-            redirect('auth/login', 'refresh');
+        $this->load->library(array('session', 'form_validation', 'pagination'));
+        $this->load->helper(array('content/content_rules'));
+        $this->load->model('ContentModel');
+        if (!$this->session->userdata('is_logged')) {
+            $this->load->library(array('session', 'form_validation', 'pagination'));
+            redirect('login');
         }
+    }
+    public function index()
+    {
+        $view = $this->load->view('content/index', '', true);
+        $this->getTemplate($view);
+    }
+    public function getTemplate($view)
+    {
+        $data = array(
+            'head' => $this->load->view('template/head', '', true),
+            'nav' => $this->load->view('template/nav', '', true),
+            'aside' => $this->load->view('template/aside', '', true),
+            'content' => $view,
+            'footer' => $this->load->view('template/footer', '', true),
+        );
+        $this->load->view('template/dashboard', $data);
+    }
+    public function create()
+    {
+        $view = $this->load->view('content/create', '', true);
+        $this->getTemplate($view);
+    }
+    public function store()
+    {
+        
     }
 }
