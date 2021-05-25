7ebe50a4b93b05ab3d35717fd6ec4812ca2ef8f9
Oscar Monsreal <A15216352@alumnos.uady.mx>
2021-05-25 11:30:49 -0500

    Model[Content]: Get and insert functions created

--- application/models/ContentModel.php
+++ application/models/ContentModel.php
@@ -4,5 +4,20 @@ class ContentModel extends CI_Model
     public function __construct(){
         $this->load->database();
     }
-    
+    public function insert($data){
+        if($this->db->insert('home_content', $data)){
+            return true;
+        }else{
+            return false;
+        }
+    }
+    public function getInfo(){
+        $sql = $this->db->get('home_content');
+        return $sql->result();
+    }
+    public function getPaginate($limit, $offset)
+    {
+        $sql = $this->db->order_by('content_id', 'DESC')->get('home_content', $limit, $offset);
+        return $sql->result();
+    }
 }
